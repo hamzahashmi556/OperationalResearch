125d684dedf29e91e59a6ff898179d93b283a64d
Hamza Alam Hashmi <hamzahalam2001@gmail.com>
2023-08-14 19:53:14 +0500

    initial Commit Xcode Projects

---
+++ OperationalResearchProject/Views/OperationalResearchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperationalResearchProjectApp.swift
+//  OperationalResearchProject
+//
+//  Created by Hamza Hashmi on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct OperationalResearchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
